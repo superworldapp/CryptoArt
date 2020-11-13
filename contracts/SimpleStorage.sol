bcc91288d6c6d680bc84090b166736218d9c8b38
Swapnil Kole <swapnilkole7500@gmail.com>
2020-11-13 15:27:12 +0530

    repo initialized

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract SimpleStorage {
+  uint storedData;
+
+  function set(uint x) public {
+    storedData = x;
+  }
+
+  function get() public view returns (uint) {
+    return storedData;
+  }
+}
