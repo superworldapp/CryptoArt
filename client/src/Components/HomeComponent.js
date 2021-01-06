5141e515967177cbe715acaa25570bd136229fdc
Swapnil Kole <swapnilkole7500@gmail.com>
2021-01-07 00:00:44 +0530

    homepage added

--- client/src/Components/HomeComponent.js
+++ client/src/Components/HomeComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import '../App.css';
 // import { Container } from 'reactstrap';
-import { Grid, Container, Card, CardContent} from '@material-ui/core';
+import { Container, Card, CardBody, CardSubtitle, CardText } from 'reactstrap';
+import { Grid } from '@material-ui/core';
 import image1 from "../images/image 15.png";
 import image2 from "../images/image 16.png";
 import image3 from "../images/image 6.png";
@@ -9,7 +10,7 @@ import image4 from "../images/image 23.png";
 import image5 from "../images/image 25.png";
 import image6 from "../images/image 28.png";
 import image7 from "../images/image 29.png";
-import svg1  from "../images/svg/angle.svg";
+import svg1 from "../images/svg/angle.svg";
 import "./HomeComponent.css"
 
 
@@ -32,26 +33,52 @@ class Home extends Component {
                         </div>
                         <div className="col2">
                             <Grid container justify="center" alignContent="center" direction="column">
-                            <img className="img2" src={image2} alt="image1" />
-                            <p className="text1"> You can SELL and BUY digital art work here. 
-                            <br/>
+                                <img className="img2" src={image2} alt="image1" />
+                                <p className="text1"> You can SELL and BUY digital art work here.
+                            <br />
                             The art work can be used in yuor Superworld</p>
                             </Grid>
-                            
+
                             <button className="start-btn">
                                 Start
-                            </button> 
+                            </button>
                         </div>
-                        <br/> <br/><br/><br/>
-                        <div style={{ display:'flex', justifyContent: 'space-between', paddingTop: '5%'}}>
-                            <p style={{fontSize:'18px', fontFamily:'Gibson', fontWeight:'bold'}}> Trending</p>
-                            <a href ="" style={{fontFamily:'Gibson', fontWeight:'550', fontSize:'18px'}}> View more <img src= {svg1} alt="svg1"/></a>
+                        <br /> <br /><br /><br />
+                        <div style={{ display: 'flex', justifyContent: 'space-between', paddingTop: '5%' }}>
+                            <p style={{ fontSize: '18px', fontFamily: 'Gibson', fontWeight: 'bold' }}> Trending</p>
+                            <a href="" style={{ fontFamily: 'Gibson', fontWeight: '550', fontSize: '18px' }}> View more <img src={svg1} alt="svg1" /></a>
                         </div>
-                        <div className= "rowImages">
-                            <Grid container justify= "space-between"direction="row" alignContent= "flex-start">
-                                
-                            <Card className="imageCards">
-                                    <CardContent>
+                        <div className="rowImages">
+                            <Grid container justify="space-between" direction="row" alignContent="flex-start">
+                                <Card>
+                                    <CardBody className="imageCards">
+                                        <img width="100%" src={image3} alt="Card image cap" />
+
+                                        <CardSubtitle tag="h6" className="mb-2 text-muted">Alimation Character</CardSubtitle>
+                                        <CardText>0.5ETH</CardText>
+
+                                    </CardBody>
+                                </Card>
+                                <Card>
+                                    <CardBody className="imageCards">
+                                        <img width="100%" src={image4} alt="Card image cap" />
+
+                                        <CardSubtitle tag="h6" className="mb-2 text-muted">Alimation Character</CardSubtitle>
+                                        <CardText>0.1ETH</CardText>
+
+                                    </CardBody>
+                                </Card>
+                                <Card>
+                                    <CardBody className="imageCards">
+                                        <img width="100%" src={image5} alt="Card image cap" />
+
+                                        <CardSubtitle tag="h6" className="mb-2 text-muted">Alimation Character</CardSubtitle>
+                                        <CardText>0.3ETH</CardText>
+
+                                    </CardBody>
+                                </Card>
+                                {/* <Card className="imageCards">
+                                    <CardBody>
                                         <Grid justify="space-around" direction="column" alignContent="center">
                                             <img className="img3" src={image3} alt="image3" />
                                             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -76,11 +103,11 @@ class Home extends Component {
 
                                             </div>
                                         </Grid>
-                                    </CardContent>
+                                    </CardBody>
                                 </Card>
 
                                 <Card className="imageCards">
-                                    <CardContent>
+                                    <CardBody>
                                         <Grid justify="space-around" direction="column" alignContent="center">
                                             <img className="img3" src={image4} alt="image4" />
                                             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -105,10 +132,10 @@ class Home extends Component {
 
                                             </div>
                                         </Grid>
-                                    </CardContent>
+                                    </CardBody>
                                 </Card>
                                 <Card className="imageCards">
-                                    <CardContent>
+                                    <CardBody>
                                         <Grid justify="space-around" direction="column" alignContent="center">
                                             <img className="img3" src={image5} alt="image3" />
                                             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -133,10 +160,10 @@ class Home extends Component {
 
                                             </div>
                                         </Grid>
-                                    </CardContent>
+                                    </CardBody>
                                 </Card>
                                 <Card className="imageCards">
-                                    <CardContent>
+                                    <CardBody>
                                         <Grid justify="space-around" direction="column" alignContent="center">
                                             <img className="img3" src={image6} alt="image3" />
                                             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -161,9 +188,9 @@ class Home extends Component {
 
                                             </div>
                                         </Grid>
-                                    </CardContent>
+                                    </CardBody>
                                 </Card> <Card className="imageCards">
-                                    <CardContent>
+                                    <CardBody>
                                         <Grid justify="space-around" direction="column" alignContent="center">
                                             <img className="img3" src={image7} alt="image3" />
                                             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -188,10 +215,10 @@ class Home extends Component {
 
                                             </div>
                                         </Grid>
-                                    </CardContent>
+                                    </CardBody>
                                 </Card>
-                            
-                            {/* <img className="img3" src={image4} alt="image4" />
+                             */}
+                                {/* <img className="img3" src={image4} alt="image4" />
                             <img className="img3" src={image5} alt="image5" />
                             <img className="img3" src={image6} alt="image6" />
                             <img className="img3" src={image7} alt="image7" />
@@ -199,14 +226,14 @@ class Home extends Component {
 
                             </Grid>
                         </div>
-                   
-                        <br/><br/><br/><br/><br/><br/><br/>
-                        </Container>
+
+                        <br /><br /><br /><br /><br /><br /><br />
+                    </Container>
 
 
                 </div>
-                <br/>
-                <br/><br/><br/><br/>
+                <br />
+                <br /><br /><br /><br />
             </>
         );
     }
