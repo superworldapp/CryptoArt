24683aae4368f01b299de0bd92060d06be862649
Arianna Campesi <Ariannafrancampesi@gmail.com>
2021-01-07 13:27:19 -0600

    fixed quotation formatting to single quotes in MyArtComponent

--- client/src/Components/MyArtComponent.js
+++ client/src/Components/MyArtComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 //import moment from 'moment';
 import {
   Button,
@@ -15,30 +15,30 @@ import {
   Modal,
   ModalHeader,
   ModalBody,
-} from "reactstrap";
-import { BrowserRouter, NavLink } from "react-router-dom";
-import Web3 from "web3";
-import { render } from "react-dom";
-import axios from "axios";
+} from 'reactstrap';
+import { BrowserRouter, NavLink } from 'react-router-dom';
+import Web3 from 'web3';
+import { render } from 'react-dom';
+import axios from 'axios';
 
-import * as aws from "aws-sdk";
-import * as dotenv from "aws-sdk";
-import * as fs from "fs";
-import * as util from "util";
-import loader from "../images/loader.svg";
-const SHA256 = require("crypto-js/sha256");
+import * as aws from 'aws-sdk';
+import * as dotenv from 'aws-sdk';
+import * as fs from 'fs';
+import * as util from 'util';
+import loader from '../images/loader.svg';
+const SHA256 = require('crypto-js/sha256');
 
-const S3 = require("aws-sdk/clients/s3");
-const AWS = require("aws-sdk");
+const S3 = require('aws-sdk/clients/s3');
+const AWS = require('aws-sdk');
 
-const path = require("path");
+const path = require('path');
 
-const BUCKET_NAME = "superworldapp";
+const BUCKET_NAME = 'superworldapp';
 
 AWS.config.update({
-  region: "us-east-1",
+  region: 'us-east-1',
   credentials: new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: "us-east-1:f7692b7a-0050-4823-9df7-1ab52e23b6c9",
+    IdentityPoolId: 'us-east-1:f7692b7a-0050-4823-9df7-1ab52e23b6c9',
   }),
 });
 const s3 = new S3();
@@ -106,21 +106,21 @@ class Allpatrender extends Component {
   };
 
   render() {
-    let but = this.props.art.isSelling ? "visible" : "invisible";
-    let bak = this.props.art.isSelling ? "bg-success text-white" : "";
+    let but = this.props.art.isSelling ? 'visible' : 'invisible';
+    let bak = this.props.art.isSelling ? 'bg-success text-white' : '';
     let pr =
-      Web3.utils.fromWei(this.props.art.tokenSellPrice.toString(), "ether") == 0
-        ? "invisible"
-        : "visible";
-    let reSellOrSell = this.props.art.isSelling ? "ReSell Item" : "Sell Item";
+      Web3.utils.fromWei(this.props.art.tokenSellPrice.toString(), 'ether') == 0
+        ? 'invisible'
+        : 'visible';
+    let reSellOrSell = this.props.art.isSelling ? 'ReSell Item' : 'Sell Item';
     return (
       <Card className={bak}>
-        <a href={this.props.art.imgUrl} target="_blank">
+        <a href={this.props.art.imgUrl} target='_blank'>
           <CardImg
             top
-            width="100%"
+            width='100%'
             src={this.props.art.imgUrl}
-            alt="Card image"
+            alt='Card image'
           />
         </a>
         <CardBody>
@@ -130,39 +130,39 @@ class Allpatrender extends Component {
           </CardText>
           <CardText className={pr}>
             <small>
-              Item Sell Price :{" "}
+              Item Sell Price :{' '}
               {Web3.utils.fromWei(
                 this.props.art.tokenSellPrice.toString(),
-                "ether"
-              )}{" "}
+                'ether'
+              )}{' '}
               ETH
             </small>
           </CardText>
           <Col sm={{ size: 12 }}>
             <Button
-              className="visible"
-              size="sm"
-              type="submit"
-              color="primary"
+              className='visible'
+              size='sm'
+              type='submit'
+              color='primary'
               onClick={this.toggleModal}
             >
               {reSellOrSell}
             </Button>
-            {"   "}
+            {'   '}
             <Button
               className={but}
-              size="sm"
-              type="submit"
-              color="primary"
+              size='sm'
+              type='submit'
+              color='primary'
               onClick={this.DeSale}
             >
               DeSell Item
             </Button>
             <Button
-              className="visible"
-              size="sm"
-              type="submit"
-              color="primary"
+              className='visible'
+              size='sm'
+              type='submit'
+              color='primary'
               onClick={this.DeSale}
             >
               Start Auction
@@ -170,38 +170,38 @@ class Allpatrender extends Component {
             <Modal
               isOpen={this.state.isModalOpen}
               toggle={this.toggleModal}
-              className="modal-md"
+              className='modal-md'
             >
-              <ModalHeader toggle={this.toggleModal} className="pl-5">
+              <ModalHeader toggle={this.toggleModal} className='pl-5'>
                 Put For Sale
               </ModalHeader>
-              <Card className="pb-5">
+              <Card className='pb-5'>
                 <CardImg
                   top
-                  width="100%"
+                  width='100%'
                   src={this.props.art.imgUrl}
-                  alt="Card image"
+                  alt='Card image'
                 />
-                <p className="m-auto p-2">
+                <p className='m-auto p-2'>
                   Art Title: {this.props.art.tokenTitle}
                 </p>
 
-                <div className="row m-auto pt-2">
+                <div className='row m-auto pt-2'>
                   <p>Art Sell Price : </p>
                   <p>
-                    {" "}
+                    {' '}
                     <Input
-                      type="text"
-                      id="sellPrice"
-                      name="sellPrice"
+                      type='text'
+                      id='sellPrice'
+                      name='sellPrice'
                       onChange={this.handleInputChange}
                     ></Input>
                   </p>
                 </div>
-                <p className="m-auto p-2">
-                  <Button type="submit" onClick={this.putForSale}>
+                <p className='m-auto p-2'>
+                  <Button type='submit' onClick={this.putForSale}>
                     Confirm
-                  </Button>{" "}
+                  </Button>{' '}
                 </p>
               </Card>
             </Modal>
@@ -221,10 +221,10 @@ class MyItemComponent extends Component {
       cust: [],
       manuf: [],
       isModalOpen1: false,
-      title: "",
-      artUrl: "",
-      price: "",
-      artHash: "",
+      title: '',
+      artUrl: '',
+      price: '',
+      artHash: '',
       nos: 0,
       isLoading: true,
     };
@@ -304,16 +304,16 @@ class MyItemComponent extends Component {
       .toLowerCase()}`;
     console.log(newfilename);
     let params = {
-      ACL: "public-read",
+      ACL: 'public-read',
       Bucket: BUCKET_NAME,
-      Key: "marketplace/" + newfilename,
+      Key: 'marketplace/' + newfilename,
       ContentType: file.type,
       Body: file,
     };
 
     s3.putObject(params, function (err, data) {
       if (err) {
-        console.log("error :", err);
+        console.log('error :', err);
       } else {
         callback(
           `https://superworldapp.s3.amazonaws.com/marketplace/${newfilename}`
@@ -325,7 +325,7 @@ class MyItemComponent extends Component {
   render() {
     const Menu = this.state.art.map((x) => {
       return (
-        <div key={x} className="col-4 col-md-3">
+        <div key={x} className='col-4 col-md-3'>
           <Allpatrender
             art={x}
             contract={this.props.contract}
@@ -337,78 +337,78 @@ class MyItemComponent extends Component {
       );
     });
 
-    let ch = "visible";
+    let ch = 'visible';
     return (
-      <div className="container">
+      <div className='container'>
         <h2>My Items</h2>
-        <Button color="success" className={ch} onClick={this.toggleModal1}>
+        <Button color='success' className={ch} onClick={this.toggleModal1}>
           Add Art
         </Button>
         <Modal
           isOpen={this.state.isModalOpen1}
           toggle={this.toggleModal1}
-          className="modal-xl"
+          className='modal-xl'
         >
           <ModalHeader toggle={this.toggleModal1}>
             <h3>Add Artwork</h3>
           </ModalHeader>
           <ModalBody>
             <Form>
-              <div className="row pl-5 pr-5">
-                <div className="col-6">
+              <div className='row pl-5 pr-5'>
+                <div className='col-6'>
                   <FormGroup>
-                    <Label htmlFor="title" className="ml-3">
+                    <Label htmlFor='title' className='ml-3'>
                       Token Title
                     </Label>
                     <Input
-                      type="text"
-                      id="title"
-                      name="title"
+                      type='text'
+                      id='title'
+                      name='title'
                       onChange={this.handleInputChange}
                     />
                   </FormGroup>
                 </div>
-                <div className="col-6">
+                <div className='col-6'>
                   <FormGroup>
-                    <Label htmlFor="price" className="ml-3">
+                    <Label htmlFor='price' className='ml-3'>
                       Item Price
                     </Label>
                     <Input
-                      type="text"
-                      id="price"
-                      name="price"
+                      type='text'
+                      id='price'
+                      name='price'
                       onChange={this.handleInputChange}
                     />
                   </FormGroup>
                 </div>
               </div>
-              <div className="row pl-5 pr-5">
-                <div className="col-6">
+              <div className='row pl-5 pr-5'>
+                <div className='col-6'>
                   <FormGroup>
-                    <Label htmlFor="nos" className="ml-3">
+                    <Label htmlFor='nos' className='ml-3'>
                       No. of Tokens
                     </Label>
                     <Input
-                      type="number"
-                      id="nos"
-                      name="nos"
+                      type='number'
+                      id='nos'
+                      name='nos'
                       onChange={this.handleInputChange}
                     />
                   </FormGroup>
                 </div>
-                <div className="col-6">
+                <div className='col-6'>
                   <FormGroup>
-                    <Label htmlFor="artHash" className="ml-3">
+                    <Label htmlFor='artHash' className='ml-3'>
                       Art
                     </Label>
-                    <Input type="file" onChange={this.fileSelectHandler} />
+                    <Input type='file' onChange={this.fileSelectHandler} />
                   </FormGroup>
                 </div>
               </div>
               <br />
-              <div className="row pl-5">
-                <div className="col-6">
-                  <Button color="primary" onClick={this.fileUploadHandler}>
+              <div className='row pl-5'>
+                <div className='col-6'>
+                  <Button color='primary' onClick={this.fileUploadHandler}>
                     Add
                   </Button>
                   {this.state.isLoading ? <img src={loader} /> : <div></div>}
@@ -420,7 +420,7 @@ class MyItemComponent extends Component {
         </Modal>
         <br />
         <br />
-        <div className="row">{Menu}</div>
+        <div className='row'>{Menu}</div>
         <br />
         <br />
         <br />
