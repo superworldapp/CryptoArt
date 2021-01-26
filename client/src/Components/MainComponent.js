b0802720d98ac09a77d324c23967a79df0269973
Arianna Campesi <Ariannafrancampesi@gmail.com>
2021-01-25 22:03:09 -0600

    added protected routes and profile info

--- client/src/Components/MainComponent.js
+++ client/src/Components/MainComponent.js
@@ -9,130 +9,128 @@ import MyItemComponent from './MyArtComponent';
 import CardDetail from './CardDetail';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Footer from './FooterComponent';
-
+import ProtectedRoute from './ProtectedRoute';
 //import HDWalletProvider from "@truffle/hdwallet-provider";
 let allDocs = [];
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            storageValue: 0,
-            web3: null,
-            accounts: null,
-            balance: 0,
-            contract: null,
-            res: null,
-            registered: 0,
-            art: null
-        };
-    }
-
-    componentDidMount = async () => {
-        try {
-            // Get network provider and web3 instance.
-            const web3 = await getWeb3();
-
-            // Use web3 to get the user's accounts.
-            const accounts = await web3.eth.getAccounts();
-            const balance = await web3.eth.getBalance(accounts[0]);
-            // Get the contract instance.
-            const networkId = await web3.eth.net.getId();
-            const deployedNetwork = BNContract.networks[networkId];
-            const instance = new web3.eth.Contract(
-                BNContract.abi,
-                deployedNetwork && deployedNetwork.address
-            );
-
-            console.log(instance);
-            this.setState({
-                web3,
-                accounts: accounts[0],
-                contract: instance,
-                balance
-            });
-            let res = await this.state.contract?.methods.tokenCount().call();
-            console.log(res);
-
-            let response = [];
-            for (let i = 1; i <= res; i++) {
-                let rex = await this.state.contract?.methods.Arts(i).call();
-                response.push(rex);
-            }
-            allDocs = [];
-            allDocs = response;
-            console.log(response);
-            this.setState({ art: allDocs });
-            
-        } catch (error) {
-            // Catch any errors for any of the above operations.
-
-            console.error(error);
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      storageValue: 0,
+      web3: null,
+      accounts: null,
+      balance: 0,
+      contract: null,
+      res: null,
+      registered: 0,
+      art: null,
     };
+  }
 
-    render() {
-        const CardWithId = ({ match }) => {
-            return (
-                <CardDetail
-                    art={
-                        this.state.art?.filter(
-                            (singleart) =>
-                                singleart.tokenIdentifier === match.params.id
-                        )[0]
-                    }
-                    contract={this.state.contract}
-                    accounts={this.state.accounts}
-                />
-            );
-        };
-        return (
-            <div className='App'>
-                <Header
-                    contract={this.state.contract}
-                    accounts={this.state.accounts}
-                    balance={this.state.balance}
-                    web3={this.state.web3}
-                />
-                <Switch>
-                    <Route
-                        exact
-                        path='/home'
-                        component={() => (
-                            <Home
-                                contract={this.state.contract}
-                                accounts={this.state.accounts}
-                            />
-                        )}
-                    />
-                    <Route
-                        exact
-                        path='/allart'
-                        component={() => (
-                            <AllItemComponent
-                                contract={this.state.contract}
-                                accounts={this.state.accounts}
-                            />
-                        )}
-                    />
-                    <Route
-                        exact
-                        path='/myart'
-                        component={() => (
-                            <MyItemComponent
-                                contract={this.state.contract}
-                                accounts={this.state.accounts}
-                            />
-                        )}
-                    />
-                    <Route path='/card/:id' component={CardWithId} />
-                    {/* <Route path='/card/:id'  location={this.state.location} key={this.state.location.key} render = {props => <CardDetail {...props} key={this.sta.location.key} /> } /> */}
+  componentDidMount = async () => {
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
 
-                    {/* <Route
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+      const balance = await web3.eth.getBalance(accounts[0]);
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = BNContract.networks[networkId];
+      const instance = new web3.eth.Contract(
+        BNContract.abi,
+        deployedNetwork && deployedNetwork.address
+      );
+
+      console.log(instance);
+      this.setState({
+        web3,
+        accounts: accounts[0],
+        contract: instance,
+        balance,
+      });
+      let res = await this.state.contract?.methods.tokenCount().call();
+      console.log(res);
+
+      let response = [];
+      for (let i = 1; i <= res; i++) {
+        let rex = await this.state.contract?.methods.Arts(i).call();
+        response.push(rex);
+      }
+      allDocs = [];
+      allDocs = response;
+      console.log(response);
+      this.setState({ art: allDocs });
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+
+      console.error(error);
+    }
+  };
+
+  render() {
+    const CardWithId = ({ match }) => {
+      return (
+        <CardDetail
+          art={
+            this.state.art?.filter(
+              (singleart) => singleart.tokenIdentifier === match.params.id
+            )[0]
+          }
+          contract={this.state.contract}
+          accounts={this.state.accounts}
+        />
+      );
+    };
+    return (
+      <div className='App'>
+        <Header
+          contract={this.state.contract}
+          accounts={this.state.accounts}
+          balance={this.state.balance}
+          web3={this.state.web3}
+        />
+        <Switch>
+          <Route
+            exact
+            path='/home'
+            component={() => (
+              <Home
+                contract={this.state.contract}
+                accounts={this.state.accounts}
+              />
+            )}
+          />
+          <ProtectedRoute
+            exact
+            path='/allart'
+            component={() => (
+              <AllItemComponent
+                contract={this.state.contract}
+                accounts={this.state.accounts}
+              />
+            )}
+          />
+          <ProtectedRoute
+            exact
+            path='/myart'
+            component={() => (
+              <MyItemComponent
+                contract={this.state.contract}
+                accounts={this.state.accounts}
+              />
+            )}
+          />
+          <Route path='/card/:id' component={CardWithId} />
+          {/* <Route path='/card/:id'  location={this.state.location} key={this.state.location.key} render = {props => <CardDetail {...props} key={this.sta.location.key} /> } /> */}
+
+          {/* <Route
                         path='/card/:id'
                         component={(props) => (
                             <CardDetail
-                            
+
                                 contract={this.state.contract}
                                 accounts={this.state.accounts}
                                 art = {this.state.art}
@@ -140,11 +138,11 @@ class Main extends Component {
                         )}
                     /> */}
 
-                    <Redirect to='/home' />
-                </Switch>
-                <Footer />
-            </div>
-        );
-    }
+          <Redirect to='/home' />
+        </Switch>
+        <Footer />
+      </div>
+    );
+  }
 }
 export default Main;
