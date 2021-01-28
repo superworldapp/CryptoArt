7c682508c105429994e7863f08423e4a3a36ff11
Arianna Campesi <Ariannafrancampesi@gmail.com>
2021-01-28 13:20:20 -0600

    added usernames to trading history

--- client/src/Components/CardDetail.js
+++ client/src/Components/CardDetail.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import TableBody from './TableBody';
 import {
-    Card,
-    CardBody,
-    CardSubtitle,
-    CardText,
-    CardImg,
-    CardHeader,
-    Table,
-    Input,
-    Modal,
-    ModalHeader
+  Card,
+  CardBody,
+  CardSubtitle,
+  CardText,
+  CardImg,
+  CardHeader,
+  Table,
+  Input,
+  Modal,
+  ModalHeader,
 } from 'reactstrap';
 import image3 from '../images/image 6.png';
 import image4 from '../images/image 23.png';
@@ -22,125 +22,133 @@ import Web3 from 'web3';
 import Axios from 'axios';
 
 const CardDetail = ({ art, accounts, contract, cre, matchId }) => {
-    console.log(art);
+  console.log(art);
 
-    const [ethPrice, setEthPrice] = useState(null);
-    const [creValue, setCreValue] = useState([]);
-    const [modalOpen, setModalOpen] = useState(false);
-    const [handleInput, setHandleInput] = useState('');
-    const [pay, setPay] = useState(0);
+  const [ethPrice, setEthPrice] = useState(null);
+  const [creValue, setCreValue] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [handleInput, setHandleInput] = useState('');
+  const [pay, setPay] = useState(0);
 
-    const ETHER = 1000000000000000000;
+  const ETHER = 1000000000000000000;
 
-    const getEthDollarPrice = () => {
-        Axios.get(
-            `https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd&include_24hr_change=true`
-        ).then((res) => {
-            // console.log(typeof res.data.ethereum.usd_24h_change);
-            setEthPrice(res.data.ethereum);
-        });
-        // return <span>{ethPrice.usd}</span>;
-    };
+  const getEthDollarPrice = () => {
+    Axios.get(
+      `https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd&include_24hr_change=true`
+    ).then((res) => {
+      // console.log(typeof res.data.ethereum.usd_24h_change);
+      setEthPrice(res.data.ethereum);
+    });
+    // return <span>{ethPrice.usd}</span>;
+  };
 
-    const handleInputChange = (event) => {
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-        setHandleInput(value);
-        console.log(handleInput);
-    };
+  const handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    setHandleInput(value);
+    console.log(handleInput);
+  };
 
-    const AddBid = async () => {
-        const res = await contract.methods.addBid(art.tokenIdentifier).send({
-            from: accounts,
-            gas: 1000000,
-            value: (handleInput * ETHER).toString()
-        });
-        console.log(res);
-    };
+  const AddBid = async () => {
+    const res = await contract.methods.addBid(art.tokenIdentifier).send({
+      from: accounts,
+      gas: 1000000,
+      value: (handleInput * ETHER).toString(),
+    });
+    console.log(res);
+  };
 
-    const getCreData = async () => {
-        let cre = await contract?.getPastEvents('tokencreated', {
-            filter: { tokenId: art.tokenIdentifier },
-            fromBlock: 0
-        });
-        //  // Using an array means OR: e.g. 20 or 23
-        let tb = await contract?.getPastEvents('tokenbought', {
-            filter: { tokenId: art.tokenIdentifier },
-            fromBlock: 0
-        });
-        let tfs = await contract?.getPastEvents('tokenputforsale', {
-            filter: { tokenId: art.tokenIdentifier },
-            fromBlock: 0
-        });
-        for (let property in cre) {
-            creValue.push(cre[property]);
-        }
-        for (let property in tb) {
-            creValue.push(tb[property]);
-        }
-        for (let property in tfs) {
-            creValue.push(tfs[property]);
-        }
+  const getCreData = async () => {
+    let cre = await contract?.getPastEvents('tokencreated', {
+      filter: { tokenId: art.tokenIdentifier },
+      fromBlock: 0,
+    });
+    //  // Using an array means OR: e.g. 20 or 23
+    let tb = await contract?.getPastEvents('tokenbought', {
+      filter: { tokenId: art.tokenIdentifier },
+      fromBlock: 0,
+    });
+    let tfs = await contract?.getPastEvents('tokenputforsale', {
+      filter: { tokenId: art.tokenIdentifier },
+      fromBlock: 0,
+    });
+    for (let property in cre) {
+      creValue.push(cre[property]);
+    }
+    for (let property in tb) {
+      creValue.push(tb[property]);
+    }
+    for (let property in tfs) {
+      creValue.push(tfs[property]);
+    }
 
-        creValue.sort((a, b) => {
-            return Number(b.returnValues.times) - Number(a.returnValues.times);
-        });
+    creValue.sort((a, b) => {
+      return Number(b.returnValues.times) - Number(a.returnValues.times);
+    });
 
-        creValue.forEach((item) => {
-            let firstChar = item.event.charAt(0).toUpperCase();
-            let restOfStr = item.event.slice(1);
-            item.event = firstChar + restOfStr;
-        });
+    creValue.forEach((item) => {
+      let firstChar = item.event.charAt(0).toUpperCase();
+      let restOfStr = item.event.slice(1);
+      item.event = firstChar + restOfStr;
+    });
 
-        console.log(creValue);
-    };
+    console.log(creValue);
+  };
 
-    const isToggleModal = () => {
-        setModalOpen(!modalOpen);
-    };
+  const isToggleModal = () => {
+    setModalOpen(!modalOpen);
+  };
 
-    const buyOrSell = () => {
-        if (art.isSelling) {
-            return (
-                <button
-                    className='btn btn-primary'
-                    onClick={async () => {
-                        const res = await contract?.methods
-                            .buyToken(art.tokenIdentifier)
-                            .send({
-                                from: accounts,
-                                value: art.tokenSellPrice,
-                                gas: 1000000
-                            });
-                        console.log(res);
-                    }}
-                    style={{
-                        width: '50%',
-                        alignSelf: 'center'
-                    }}>
-                    Buy Item
-                </button>
-            );
-        } else if (art.auction.isBidding) {
-            return (
-                <React.Fragment>
-                    <Input
-                        type='text'
-                        id='sellPrice'
-                        name='sellPrice'
-                        onChange={handleInputChange}></Input>
-                    <button
-                        className='btn-primary'
-                        color='primary'
-                        onClick={AddBid}
-                        style={{
-                            width: '50%',
-                            alignSelf: 'center'
-                        }}>
-                        Place Bid
-                    </button>
-                    {/* <Modal
+  const buyOrSell = () => {
+    if (art.isSelling) {
+      return (
+        <button
+          className='btn btn-primary'
+          onClick={async () => {
+            const res = await contract?.methods
+              .buyToken(art.tokenIdentifier)
+              .send({
+                from: accounts,
+                value: art.tokenSellPrice,
+                gas: 1000000,
+              });
+            console.log(res);
+          }}
+          style={{
+            width: '50%',
+            alignSelf: 'center',
+          }}
+        >
+          Buy Item
+        </button>
+      );
+    } else if (art.auction.isBidding) {
+      return (
+        <React.Fragment>
+          <Input
+            type='text'
+            id='sellPrice'
+            name='sellPrice'
+            style={{
+              width: '50%',
+              alignSelf: 'center',
+              marginBottom: '0.5rem',
+            }}
+            onChange={handleInputChange}
+          ></Input>
+          <button
+            className='btn-primary'
+            color='primary'
+            onClick={AddBid}
+            style={{
+              width: '50%',
+              alignSelf: 'center',
+            }}
+          >
+            Place Bid
+          </button>
+          {/* <Modal
                         isOpen={modalOpen}
                         toggle={isToggleModal}
                         className='modal_popup'>
@@ -279,124 +287,118 @@ const CardDetail = ({ art, accounts, contract, cre, matchId }) => {
                             </CardBody>
                         </Card>
                     </Modal> */}
-                </React.Fragment>
-            );
-        }
-    };
+        </React.Fragment>
+      );
+    }
+  };
 
-    useEffect(() => {
-        getCreData();
-        getEthDollarPrice();
-        // console.log(matchId)
-        // console.log('Header', myProperties);
-    }, []);
+  useEffect(() => {
+    getCreData();
+    getEthDollarPrice();
+    // console.log(matchId)
+    // console.log('Header', myProperties);
+  }, []);
 
-    //   const buyItem = async () => {
-    //         const res = await contract.methods
-    //             .buyToken(art.tokenIdentifier)
-    //             .send({
-    //                 from: accounts,
-    //                 value: art.tokenSellPrice,
-    //                 gas: 1000000
-    //             });
-    //         console.log(res);
-    //     };
-    return (
-        <>
-            <div className='container'>
-                <div className='d-flex justify-content-between mt-5 py-5'>
-                    <div
-                        className='card'
-                        style={{
-                            width: '50%'
-                        }}>
-                        <a href={art?.imgurl} target='_blank'>
-                            <img
-                                src={art?.imgurl}
-                                className='card-img'
-                                alt='...'
-                            />
-                        </a>
-                    </div>
-                    <div className='information d-flex flex-column'>
-                        {/* <a href='#'>{match.params.id}</a>
+  //   const buyItem = async () => {
+  //         const res = await contract.methods
+  //             .buyToken(art.tokenIdentifier)
+  //             .send({
+  //                 from: accounts,
+  //                 value: art.tokenSellPrice,
+  //                 gas: 1000000
+  //             });
+  //         console.log(res);
+  //     };
+  return (
+    <>
+      <div className='container'>
+        <div className='d-flex justify-content-between mt-5 py-5'>
+          <div
+            className='card'
+            style={{
+              width: '50%',
+            }}
+          >
+            <a href={art?.imgurl} target='_blank'>
+              <img src={art?.imgurl} className='card-img' alt='...' />
+            </a>
+          </div>
+          <div className='information d-flex flex-column'>
+            {/* <a href='#'>{match.params.id}</a>
                         <h1>{match.params.id}</h1> */}
-                        <p>
-                            Owned by{' '}
-                            <span class='text-primary'>
-                                {art?.tokenCreator}
-                            </span>
-                        </p>
-                        <div
-                            className='card py-3'
-                            style={{
-                                width: '30rem'
-                            }}>
-                            <p className='text-secondary'>Current price</p>
-                            <h4>
-                                {Web3.utils.fromWei(
-                                    art?.tokenSellPrice.toString(),
-                                    'ether'
-                                )}{' '}
-                                ETH
-                                <small>
-                                    <span className='text-secondary'>
-                                        (
-                                        {(
-                                            Web3.utils.fromWei(
-                                                art?.tokenSellPrice.toString(),
-                                                'ether'
-                                            ) * ethPrice?.usd
-                                        ).toFixed(2)}{' '}
-                                        USD)
-                                    </span>
-                                </small>
-                            </h4>
+            <p>
+              Owned by <span class='text-primary'>{art?.tokenCreator}</span>
+            </p>
+            <div
+              className='card py-3'
+              style={{
+                width: '30rem',
+              }}
+            >
+              <p className='text-secondary'>Current price</p>
+              <h4>
+                {Web3.utils.fromWei(art?.tokenSellPrice.toString(), 'ether')}{' '}
+                ETH
+                <small>
+                  <span className='text-secondary'>
+                    (
+                    {(
+                      Web3.utils.fromWei(
+                        art?.tokenSellPrice.toString(),
+                        'ether'
+                      ) * ethPrice?.usd
+                    ).toFixed(2)}{' '}
+                    USD)
+                  </span>
+                </small>
+              </h4>
 
-                            {buyOrSell()}
-                        </div>
-                    </div>
-                </div>
-                <div className='my-5'>
-                    <Card
-                        className='card'
-                        style={{
-                            overflow: 'auto'
-                        }}>
-                        <CardHeader
-                            className='text-left'
-                            style={{
-                                backgroundColor: '#fff'
-                            }}>
-                            <h4>
-                                <i class='fas fa-arrows-alt-v'></i> Trading
-                                History
-                            </h4>
-                        </CardHeader>
-                        <React.Fragment>
-                            <CardBody>
-                                <Table
-                                    style={{
-                                        width: '100%'
-                                    }}>
-                                    <thead>
-                                        <tr className='text-secondary'>
-                                            <th>Event</th>
-                                            <th>Price</th>
-                                            <th>From</th>
-                                            <th>To</th>
-                                            <th>Date</th>
-                                        </tr>
-                                    </thead>
-                                    <TableBody cre={creValue} />
-                                </Table>
-                            </CardBody>
-                        </React.Fragment>
-                    </Card>
-                </div>
+              {buyOrSell()}
             </div>
-        </>
-    );
+          </div>
+        </div>
+        <div className='my-5'>
+          <Card
+            className='card'
+            style={{
+              overflow: 'auto',
+            }}
+          >
+            <CardHeader
+              className='text-left'
+              style={{
+                backgroundColor: '#fff',
+              }}
+            >
+              <h4>
+                <i class='fas fa-arrows-alt-v'></i> Trading History
+              </h4>
+            </CardHeader>
+            <React.Fragment>
+              <CardBody>
+                <Table
+                  style={{
+                    width: '100%',
+                  }}
+                >
+                  <thead>
+                    <tr className='text-secondary'>
+                      <th>Event</th>
+                      <th>Price</th>
+                      <th>From</th>
+                      <th>To</th>
+                      <th>Date</th>
+                    </tr>
+                  </thead>
+                  <TableBody cre={creValue} />
+                </Table>
+              </CardBody>
+            </React.Fragment>
+          </Card>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CardDetail;
--- client/src/Components/TableBody.js
+++ client/src/Components/TableBody.js
@@ -1,55 +1,63 @@
-import React from 'react'
+import React from 'react';
 import Web3 from 'web3';
 
-const TableBody = ({cre}) => {
-    return (
-        <tbody>
-            {cre?.map((item) => {
-                return (
-                    <React.Fragment>
-                        <tr key={item.id}>
-                            <th scope='row'>{item?.event}</th>
-                            <td>
-                                {item?.returnValues.tokenPrice
-                                    ? `${Web3.utils.fromWei(
-                                          Number(
-                                              item?.returnValues.tokenPrice
-                                          ).toString(),
-                                          'ether'
-                                      )} ETH`
-                                    : item?.returnValues.sellPrice
-                                    ? `${Web3.utils.fromWei(
-                                          Number(
-                                              item?.returnValues.sellPrice
-                                          ).toString(),
-                                          'ether'
-                                      )} ETH`
-                                    : 'null'}
-                            </td>
-                            <td>
-                                {item?.returnValues.tokenCreator
-                                    ? item?.returnValues.tokenCreator
-                                    : item?.returnValues.seller}
-                            </td>
-                            <td>
-                                {item?.returnValues.newowner
-                                    ? item?.returnValues.newowner
-                                    : 'null'}
-                            </td>
-                            <td>
-                                {Math.round(
-                                    (Date.now() / 1000 -
-                                        item?.returnValues.times) /
-                                        60
-                                )}{' '}
-                                minutes ago
-                            </td>
-                        </tr>
-                    </React.Fragment>
-                );
-            })}
-        </tbody>
-    );
-}
+const TableBody = ({ cre }) => {
+  const accUsername = (accNum) => {
+    if (accNum === '0xB4C33fFc72AF371ECaDcF72673D5644B24946256')
+      return '@Chitra';
+    else if (accNum === '0x0d5567345D3Cb1114471BC07c396Cc32C7CF92ec')
+      return '@Arianna';
+    else if (accNum === '0xABD82c9B735F2C89f2e62152A9884F4A92414F20')
+      return '@CJMain';
+    else if (accNum === '0x63611F92FA2d7B7e6625a97E6474b7fA16DbD89F')
+      return '@CJ Test';
+    else if (accNum === '0x4271AC6Bb565D120e2Ac1C3fb855aE5Dad6aE8ff')
+      return '@Swapnil';
+    else return '@Annonymous';
+  };
 
-export default TableBody
+  return (
+    <tbody>
+      {cre?.map((item) => {
+        return (
+          <React.Fragment>
+            <tr key={item.id}>
+              <th scope='row'>{item?.event}</th>
+              <td>
+                {item?.returnValues.tokenPrice
+                  ? `${Web3.utils.fromWei(
+                      Number(item?.returnValues.tokenPrice).toString(),
+                      'ether'
+                    )} ETH`
+                  : item?.returnValues.sellPrice
+                  ? `${Web3.utils.fromWei(
+                      Number(item?.returnValues.sellPrice).toString(),
+                      'ether'
+                    )} ETH`
+                  : 'null'}
+              </td>
+              <td>
+                {item?.returnValues.tokenCreator
+                  ? accUsername(item?.returnValues.tokenCreator)
+                  : accUsername(item?.returnValues.seller)}
+              </td>
+              <td>
+                {item?.returnValues.newowner
+                  ? accUsername(item?.returnValues.newowner)
+                  : 'null'}
+              </td>
+              <td>
+                {Math.round(
+                  (Date.now() / 1000 - item?.returnValues.times) / 60
+                )}{' '}
+                minutes ago
+              </td>
+            </tr>
+          </React.Fragment>
+        );
+      })}
+    </tbody>
+  );
+};
+
+export default TableBody;
