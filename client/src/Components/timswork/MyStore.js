82ddbac2e15504c26bfa78bcad72cab96e3d6b33
Tim La <timla111@Tims-Air-2.fios-router.home>
2021-03-14 20:56:15 -0400

    changed state to queue

--- client/src/Components/timswork/MyStore.js
+++ client/src/Components/timswork/MyStore.js
@@ -11,10 +11,12 @@ class MyStore1 extends Component {
         this.state = {
             isModalOpen: false,
             isListModalOpen: false,
-            artStatus: 'Active',
+            isEditModalOpen: false,
+            artStatus: 'Queue',
         }
         this.toggleModal = this.toggleModal.bind(this);
         this.listToggleModal = this.listToggleModal.bind(this);
+        this.editToggleModal = this.editToggleModal.bind(this);
         this.storeQueue = this.storeQueue.bind(this)
         this.storeActive = this.storeActive.bind(this);
 
@@ -29,6 +31,12 @@ class MyStore1 extends Component {
     listToggleModal() {
         this.setState({
             isListModalOpen: !this.state.isListModalOpen,
+        });
+    }
+
+    editToggleModal() {
+        this.setState({
+            isEditModalOpen: !this.state.isEditModalOpen,
         });
     }
 
@@ -84,7 +92,7 @@ class MyStore1 extends Component {
                                     <img src={test2img } alt='art-1'/>
                                     <div className='mystore-art-caption'>
                                         <h2>Leopard</h2>
-                                        <span className='mystore-art-caption-line2'><p>1.00 ETH</p><button onClick={this.toggleModal} className='mystore-edit-btn'>Edit</button></span>
+                                        <span className='mystore-art-caption-line2'><p>1.00 ETH</p><button onClick={this.editToggleModal} className='mystore-edit-btn'>Edit</button></span>
                                         <p className='mystore-art-caption-usd'>($1,580.10 USD)</p>
                                     </div>
                                 </div>
@@ -122,23 +130,50 @@ class MyStore1 extends Component {
                     )
                 }
             {/* */}
+
+            {/* Upload Modal */}
                 {this.state.isListModalOpen && (
                     <div className='mystore-modal-bg'>
                         <div className='upload-modal'>
                             <div className='upload-modal-container'>
-                            <button onClick={this.listToggleModal} className='upload-modal-close-btn'>X</button>
-                            <h3>UPLOAD TO MYSTORE</h3>
-                            <p>Image, Video, Audio or 3D Model</p>
-                            <span className='upload-modal-line1'><h4>File to Upload*</h4><button>Browse...</button><p>Leopard.png</p></span>
-                            <span className='upload-modal-line2'><h4>Name*</h4><input type='text' placeholder='Leopard'/></span>
-                            <span className='upload-modal-line3'><h4>Description</h4><input type='text' placeholder='Leopard'/></span>
-                            <span className='upload-modal-line4'><h4>No. of Tokens</h4><input type='text' placeholder='Leopard'/></span>
-                            <button className='upload-modal-btn'>Confirm</button>
+                                <button onClick={this.listToggleModal} className='upload-modal-close-btn'>X</button>
+                                <h3>UPLOAD TO MYSTORE</h3>
+                                <p>Image, Video, Audio or 3D Model</p>
+                                <span className='upload-modal-line1'><h4>File to Upload*</h4><button>Browse...</button><p>Leopard.png</p></span>
+                                <span className='upload-modal-line2'><h4>Name*</h4><input type='text' placeholder='Leopard'/></span>
+                                <span className='upload-modal-line3'><h4>Description</h4><input type='text' placeholder='Leopard'/></span>
+                                <span className='upload-modal-line4'><h4>No. of Tokens</h4><input type='text' placeholder='Leopard'/></span>
+                                <button className='upload-modal-btn'>Confirm</button>
                             </div>
                         </div>
                     </div>
                 )}
-            {/* Upload Modal */}
+            {/* */}
+
+            {/* Edit Modal */}
+                {this.state.isEditModalOpen && (
+                    <div className='mystore-modal-bg'>
+                    <div className='mystore-modal edit-modal'>
+                        <div className='mystore-resale-container edit-modal-container'>
+                                <button onClick={this.editToggleModal} className='mystore-edit-close-btn'>X</button>
+                                <h3>EDIT LISTING</h3>
+                                <p className='mystore-resale-description'>Image, Video, Audio or 3D Model</p>
+                                <div className='mystore-body-container'>
+                                    <span className='mystore-body-line-1'><h4>File</h4><p className='mystore-body-line-1-p'>BackCountry...png</p></span>
+                                    <span className='mystore-body-line-2'><h4>Auction</h4><input type='checkbox' className='mystore-body-line-2-checkbox'></input></span>
+                                    <span className='mystore-body-line-3'><h4>Buy Now</h4><input type='checkbox' className='mystore-body-line-3-checkbox'></input></span>
+                                    <span className='mystore-body-line-4'><h4>Token Price*</h4><input className='mystore-token-price'></input><p className='mystore-token-type'>ETH &nbsp;</p><p className='mystore-token-usd'>($1,580.10 USD)</p></span>
+                                    <p className='mystore-body-line-4-subtitle'>(State Price if Auction)</p>
+                                    <span className='mystore-body-line-5'><h4>Duration*</h4><input className='mystore-token-days'></input><p className='mystore-token-duration-subtitle'>Days</p></span>
+                                    <button className='mystore-relist-btn'>Relist</button>
+                                    <button className='mystore-takedown-btn'>Take Down</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )}
+
+            {/* */}
 
            
         </div>
