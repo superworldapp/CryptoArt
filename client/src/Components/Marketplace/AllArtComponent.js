b2e3b8235dd8f10b4bf6c00becd59b6f924ec61d
Viktor-Web-Dev <viktorwebdeveloper@gmail.com>
2021-03-30 18:25:18 +0300

    feat(marketplace): add chips, add img, name of artist, title for images

--- client/src/Components/Marketplace/AllArtComponent.js
+++ client/src/Components/Marketplace/AllArtComponent.js
@@ -1,148 +1,101 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import CreationCards from "./CreationCards";
 import SimpleMenu from "./menu/MenuListed";
 import CreateFilterList from "./CreateFilterList";
+import CreateChipList from "./CreateChipList";
 
-import profile from '../../images/svg/avatar.svg';
-import coverImage from '../../images/arcDeTriomphe.jpeg';
 import burger from '../../images/svg/burger-recently-list.svg';
 import arrow from '../../images/svg/arrow.svg';
 import './AllArtComponent.scss';
 
 const AllItemComponent = (props) => {
-	console.log('========>props', props);
-	const mockArr = [
-		{
-			cardImage: coverImage,
-			profileImage: profile,
-			cardTitle: 'Alimation Character',
-			CardSubtitle: 'by',
-			CardSubtitleName: 'Amelia',
-			price: '0.5ETH',
-			usdPrice: '($985.56 USD)',
-			time: '26 hrs 42 mins remaining',
-		},
-		{
-			cardImage: coverImage,
-			profileImage: profile,
-			cardTitle: 'Alimation Character',
-			CardSubtitle: 'by',
-			CardSubtitleName: 'Amelia',
-			price: '0.5ETH',
-			usdPrice: '($985.56 USD)',
-			time: '26 hrs 42 mins remaining',
-		},
-		{
-			cardImage: coverImage,
-			profileImage: profile,
-			cardTitle: 'Alimation Character',
-			CardSubtitle: 'by',
-			CardSubtitleName: 'Amelia',
-			price: '0.5ETH',
-			usdPrice: '($985.56 USD)',
-			time: '26 hrs 42 mins remaining',
-		},
-		{
-			cardImage: coverImage,
-			profileImage: profile,
-			cardTitle: 'Alimation Character',
-			CardSubtitle: 'by',
-			CardSubtitleName: 'Amelia',
-			price: '0.5ETH',
-			usdPrice: '($985.56 USD)',
-			time: '26 hrs 42 mins remaining',
-		},
-		{
-			cardImage: coverImage,
-			profileImage: profile,
-			cardTitle: 'Alimation Character',
-			CardSubtitle: 'by',
-			CardSubtitleName: 'Amelia',
-			price: '0.5ETH',
-			usdPrice: '($985.56 USD)',
-			time: '26 hrs 42 mins remaining',
-		},
-		{
-			cardImage: coverImage,
-			profileImage: profile,
-			cardTitle: 'Alimation Character',
-			CardSubtitle: 'by',
-			CardSubtitleName: 'Amelia',
-			price: '0.5ETH',
-			usdPrice: '($985.56 USD)',
-			time: '26 hrs 42 mins remaining',
-		},
-		{
-			cardImage: coverImage,
-			profileImage: profile,
-			cardTitle: 'Alimation Character',
-			CardSubtitle: 'by',
-			CardSubtitleName: 'Amelia',
-			price: '0.5ETH',
-			usdPrice: '($985.56 USD)',
-			time: '26 hrs 42 mins remaining',
-		},
-		{
-			cardImage: coverImage,
-			profileImage: profile,
-			cardTitle: 'Alimation Character',
-			CardSubtitle: 'by',
-			CardSubtitleName: 'Amelia',
-			price: '0.5ETH',
-			usdPrice: '($985.56 USD)',
-			time: '26 hrs 42 mins remaining',
-		},
-	]
-
 	const mockList = [
 		{
 			name: "STATUS",
-			lists: [{name: 'On Auction'}, {name: 'Buy Now'}, {name: 'Make Offer'}],
+			lists: [{name: 'On Auction', key: 0}, {name: 'Buy Now', key: 1}, {name: 'Make Offer', key: 2}],
 			id: 1,
 		},
 		{
 			name: "TRENDING",
-			lists: [{name: 'Text'}, {name: 'Text'}, {name: 'Text'}, {name: 'Text'}],
+			lists: [{name: 'Text', key: 3}, {name: 'Text', key: 4}, {name: 'Text', key: 5}, {name: 'Text', key: 6}],
 			id: 2,
 		},
 		{
 			name: "Type",
-			lists: [{name: 'Images'}, {name: 'GIF'}, {name: 'Video'}],
+			lists: [{name: 'Images', key: 7}, {name: 'GIF', key: 8}, {name: 'Video', key: 9}],
 			id: 3,
 		},
 	];
 
+	const [checked, setChecked] = useState([])
+
+	const handleRemove = (elem) => {
+		setChecked(checked.filter(el => el !== elem))
+	}
+
+	const handleRemoveAll = () => {
+		setChecked([])
+	}
+
+	const handleCheck = (elem) => {
+		if (checked.filter(el => el.key === elem.key && el.name === elem.name).length > 0) {
+			setChecked(checked.filter(el => el.key !== elem.key && el.name !== elem.name))
+		} else {
+			setChecked(checked.concat(elem))
+		}
+	}
+
 	return (
 		<div className="container_marketplace">
 			<div className="filter_list">
-				<CreateFilterList list={mockList}/>
+				<CreateFilterList
+					list={mockList}
+					checked={checked}
+					handleCheck={handleCheck}
+				/>
 			</div>
-			<div className="cards_marketplace">
-				<div className="head_result">
-					<p className="head_result_num">{props.batch.length} Results</p>
-					<p className="head_result_menu">
-						<img
-							className="burger_img"
-							src={burger}
-							alt="burger"
+			<divc className="chipCards">
+				{checked.length > 0 &&
+				<div className="chipList">
+					<div className="chipHead">Filters</div>
+					<div className="chips">
+						<CreateChipList
+							checked={checked}
+							handleRemove={handleRemove}
+							handleRemoveAll={handleRemoveAll}
 						/>
-						<SimpleMenu/>
-						<img
-							className="arrow_img"
-							src={arrow}
-							alt="arrow"
-						/>
-					</p>
-				</div>
-				<div className="cards_wrapper">
-					<div className="creation_cards">
-						{mockArr.map((item) => (
-							<CreationCards props={props} {...item}/>
-						))}
 					</div>
 				</div>
-			</div>
+				}
+				<div className="cards_marketplace">
+					<div className="head_result">
+						<p className="head_result_num">
+							{props.batch.length} Results
+						</p>
+						<p className="head_result_menu">
+							<img
+								className="burger_img"
+								src={burger}
+								alt="burger"
+							/>
+							<SimpleMenu/>
+							<img
+								className="arrow_img"
+								src={arrow}
+								alt="arrow"
+							/>
+						</p>
+					</div>
+					<div className="cards_wrapper">
+						<div className="creation_cards">
+							{props.batch.map((item) => (
+								<CreationCards props={props} {...item}/>
+							))}
+						</div>
+					</div>
+				</div>
+			</divc>
 		</div>
 	);
 }
--- client/src/Components/Marketplace/CreateFilterList.js
+++ client/src/Components/Marketplace/CreateFilterList.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { Checkbox } from "@material-ui/core";
-import { withStyles } from '@material-ui/core/styles';
+import React from 'react';
+import {Checkbox} from "@material-ui/core";
+import {withStyles} from '@material-ui/core/styles';
 
 import './CreateFilterList.scss'
 
@@ -14,48 +14,28 @@ const CustomCheckbox = withStyles({
 	checked: {},
 })((props) => <Checkbox color="default" {...props} />);
 
-const CreateFilterList = ({ list }) => {
-	const [state, setState] = useState({
-		onAuction: false,
-		buyNow: false,
-		makeOffer: false,
-		text: false,
-		text1: false,
-		text2: false,
-		text3: false,
-		images: false,
-		gif: false,
-		video: false,
-	});
-
-	const handleChange = (event) => {
-		const { name, checked } = event.target;
-		setState({ ...state, [name]: checked });
-	};
-
-	return (
-		<>
-			{list.map(({name, lists}) => (
-				<div className="filter_wrapper">
-					<p className="filter_head">{name}</p>
-					{lists.map(({name, id}, index) => (
-						<div className="form-check">
-								<label className="form-check-label" htmlFor={index+name}>
-									<CustomCheckbox
-										checked={state[id]}
-										name={name}
-										onChange={handleChange}
-										color="primary"
-										inputProps={{ 'aria-label': 'secondary checkbox' }}
-									/>
-									{name}
-								</label>
-						</div>
-					))}
-				</div>
-			))}
-		</>
-	);
-};
+const CreateFilterList = ({list, handleCheck, checked}) => (
+	<>
+		{list.map(({name, lists}) => (
+			<div className="filter_wrapper">
+				<p className="filter_head">{name}</p>
+				{lists.map((elem) => (
+					<div className="form-check">
+						<label className="form-check-label" htmlFor={elem.key + name}>
+							<CustomCheckbox
+								checked={checked.filter(el => el.key === elem.key && el.name === elem.name).length > 0}
+								name={elem.name}
+								onChange={() => handleCheck(elem)}
+								color="primary"
+								inputProps={{'aria-label': 'secondary checkbox'}}
+							/>
+							{elem.name}
+						</label>
+					</div>
+				))}
+			</div>
+		))}
+	</>
+);
 
 export default CreateFilterList;
