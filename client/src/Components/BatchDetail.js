bc6a6b12819c4dbbb19161614de61c05c899b08c
Viktor-Web-Dev <viktorwebdeveloper@gmail.com>
2021-04-02 14:12:25 +0300

    feat(batchDetail): add button show more for cards and styles

--- client/src/Components/BatchDetail.js
+++ client/src/Components/BatchDetail.js
@@ -27,10 +27,13 @@ import loader from '../images/loader.svg';
 import anonUser from '../images/user.png';
 import heart from '../images/svg/batchHeart.svg';
 import avatar from '../images/svg/batchAvatar.svg';
+import heartCard from '../images/svg/heartSvg.svg';
+import profileCard from '../images/svg/avatar.svg';
 import "./BatchDetail.scss";
 // import heart from "../images/svg/Heart.svg";
 import dropdownarrow from "../assets/svg/Drop down arrow.svg";
 import Web3 from 'web3';
+
 const ETHER = 1000000000000000000;
 export const cardpills = [
   {
@@ -228,6 +231,26 @@ class Allpatrender extends Component {
       .send({ from: this.props.accounts, gas: 7000000 });
     console.log(res);
   };
+
+  accUsernameCard = (accNum) => {
+    if (accNum === '0xB4C33fFc72AF371ECaDcF72673D5644B24946256')
+      return '@Chitra';
+    else if (accNum === '0x0d5567345D3Cb1114471BC07c396Cc32C7CF92ec')
+      return '@Arianna';
+    else if (accNum === '0xABD82c9B735F2C89f2e62152A9884F4A92414F20')
+      return '@CJMain';
+    else if (accNum === '0x63611F92FA2d7B7e6625a97E6474b7fA16DbD89F')
+      return '@CJ Test';
+    else if (accNum === '0x4271AC6Bb565D120e2Ac1C3fb855aE5Dad6aE8ff')
+      return '@Swapnil';
+    else if (accNum === '0x81B2362F55Ea93f71990d7F446dca80BdD94C6e7')
+      return '@SwapnilTest';
+    else if (accNum === '0xE337525DD5d34fC683f43CbBDF3e1EDe0833B744')
+      return '@Viktor';
+    else if (accNum === '0x32c93d70E9813fFe62a2fCf6189F14A4ff2e8cB3')
+      return '@Alex';
+    else return '@Annonymous';
+  };
   render() {
     let but = this.props.art._isSellings ? ' ' : 'hidden';
     let bak = this.props.art._isSellings ? 'bg-success text-white' : '';
@@ -280,7 +303,8 @@ class Allpatrender extends Component {
     };
     img.src = this.props.BatchCreated._imgurl;
     img.onload();
-
+console.log(`==========>this.props.art`, this.props.art);
+console.log(`==========>this.props.BatchCreated`, this.props.BatchCreated);
     return (
 
       <div>
@@ -292,30 +316,82 @@ class Allpatrender extends Component {
         }}
         onClick={this.toggleArtModal}
       > */}
-        <Card className='mycollection-card'>
-        <Link
-                    style={{
-                        color: '#212529',
-                        textDecoration: 'none'
-                    }}
-                    to={`/card/${this.props.art._refbatch}`}>
+        <Card className="cardBatchWrapper">
+          <Link
+            style={{
+              color: '#212529',
+              textDecoration: 'none'
+            }}
+            to={`/card/${this.props.art._refbatch}`}>
           <CardImg
             top
-            // src={this.props.art._imgurl}
+            className="card-background-image"
             src={this.props.BatchCreated._imgurl}
-            alt='Card image'
-            style={{
-              width: '98.5%',
-            }}
-          ></CardImg>
-          <CardSubtitle>Bid Price : {this.props.art._bidprice}</CardSubtitle>
-          <CardSubtitle>isBidding? : {this.props.art._isBidding}</CardSubtitle>
-          <CardSubtitle>isSell?{this.props.art._isSellings}</CardSubtitle>
-          <CardSubtitle>sellprice = {this.props.art._sellprice}</CardSubtitle>
-          <CardSubtitle><small>tokenowner = {this.props.art._tokenOwner}</small></CardSubtitle>
-          
+            alt='image3'
+          />
+          <CardImgOverlay className="cardImgOverlay">
+            <div className="userImg">
+              <img src={this.props.profileImage || profileCard} alt="userImg"/>
+            </div>
+            <div className="card-user-heart">
+              <img src={heartCard} alt="heart"/>
+            </div>
+            <CardTitle className="card-user-title">
+              {this.props.BatchCreated._tokenBatchName || 'none'}
+            </CardTitle>
+          </CardImgOverlay>
+          <CardBody className="card-body">
+            <div className="card-user-body">
+              <CardSubtitle className="card-created-by">
+                by
+              </CardSubtitle>
+              <CardSubtitle className="card-subtitle-name">
+                {this.accUsernameCard(this.props.BatchCreated._tokenCreator) || ''}
+              </CardSubtitle>
+            </div>
+            <div className='card-text-info'>
+              <CardText className="card-text-info-price">
+                {this.props.price || '0.5ETH'}
+                <p className="card-text-info-usd">
+                  {this.props.usdPrice || '($985.56 USD)'}
+                </p>
+              </CardText>
+              <div>
+                <button className='card-buy-button'>Bid</button>
+              </div>
+            </div>
+            <div className='card-buy-time'>
+              <p className='card-buy-time-text'>
+                {this.props.time || '26 hrs 42 mins remaining'}
+              </p>
+            </div>
+          </CardBody>
           </Link>
-      </Card>
+        </Card>
+      {/*  <Card className='mycollection-card'>*/}
+      {/*  <Link*/}
+      {/*              style={{*/}
+      {/*                  color: '#212529',*/}
+      {/*                  textDecoration: 'none'*/}
+      {/*              }}*/}
+      {/*              to={`/card/${this.props.art._refbatch}`}>*/}
+      {/*    <CardImg*/}
+      {/*      top*/}
+      {/*      // src={this.props.art._imgurl}*/}
+      {/*      src={this.props.BatchCreated._imgurl}*/}
+      {/*      alt='Card image'*/}
+      {/*      style={{*/}
+      {/*        width: '98.5%',*/}
+      {/*      }}*/}
+      {/*    ></CardImg>*/}
+      {/*    <CardSubtitle>Bid Price : {this.props.art._bidprice}</CardSubtitle>*/}
+      {/*    <CardSubtitle>isBidding? : {this.props.art._isBidding}</CardSubtitle>*/}
+      {/*    <CardSubtitle>isSell?{this.props.art._isSellings}</CardSubtitle>*/}
+      {/*    <CardSubtitle>sellprice = {this.props.art._sellprice}</CardSubtitle>*/}
+      {/*    <CardSubtitle><small>tokenowner = {this.props.art._tokenOwner}</small></CardSubtitle>*/}
+
+      {/*    </Link>*/}
+      {/*</Card>*/}
       {/* </button> */}
 
       {/* Art Modal */}
@@ -415,6 +491,7 @@ class BatchDetail extends Component{
     super(props);
     this.state = {
       readMore: true,
+      viewMore: false
     }
 this.accUsername = this.accUsername.bind(this);
 //this.getCreData = this.getCreData.bind(this);
@@ -464,15 +541,18 @@ this.accUsername = this.accUsername.bind(this);
 
 render(){
   let tokenInBatch;
-
+console.log(`==========>this.props`, this.props);
     //getCreData();
     
      console.log(this.props.BatchCreated[0])
      let batchID = this.props.matchId; 
      tokenInBatch = this.props.allTokens.filter((x) => x._refbatch == batchID);
      console.log('Header', tokenInBatch);
-     
-     const Menu = tokenInBatch.map((x) => {
+      let newTokenBatch
+     if(!this.state.viewMore){
+       newTokenBatch = tokenInBatch.slice(0,15)
+     }
+     const Menu = (this.state.viewMore ? tokenInBatch : newTokenBatch).map((x) => {
       return (
         <div key={1}>
           <Allpatrender
@@ -481,8 +561,6 @@ render(){
             accounts={this.props.accounts}
             BatchCreated={this.props.BatchCreated[0]}
           />
-          <br />
-          <br />
         </div>
       );
     });
@@ -544,7 +622,12 @@ render(){
         <div className="batchCards">
           <div className="batchCardsHead">
             <div className="batchToken">All Tokens</div>
-            <div className="batchViewMore">View More</div>
+            <div className="batchViewMore">{tokenInBatch.length > 15
+              ? <div onClick={() => this.setState({viewMore: !this.state.viewMore})}>
+                View More
+              </div>
+              : ''}
+            </div>
           </div>
           <div className="batchCards">
             {Menu}
